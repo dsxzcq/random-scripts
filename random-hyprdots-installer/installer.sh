c976d321a76703dadd8ada3110db2caf5b5a4b78
dsx <100709830+dsxzcq@users.noreply.github.com>
2025-02-14 23:13:54 -0600

    Update installer.sh

--- random-hyprdots-installer/installer.sh
+++ random-hyprdots-installer/installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-DOWNLOADERS_FILE="https://raw.githubusercontent.com/dsxzcq/random-scripts/main/random-hyprdots-installer/downloaders.json"
-lang_file="https://raw.githubusercontent.com/dsxzcq/random-scripts/main/random-hyprdots-installer/langs.json"
+DOWNLOADERS_FILE="https://raw.githubusercontent.com/dsxzcq/random-scripts/main/rare-jsons/downloaders.json"
+lang_file="https://raw.githubusercontent.com/dsxzcq/random-scripts/main/rare-jsons/rha-langs.json"
 
 function get_string() {
     echo "$lang_strings" | jq -r ".$1"
